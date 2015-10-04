a2d7a75f4ac169ec78fa93de5f5872ca10884d3c
Björn Bartels <bartels@dragon-projects.net>
2015-10-04 18:42:32 +0200

    update layout add nav partial

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ return array(
     ),
     'template_map' => array(
         'layout/layout'           => __DIR__ . '/view/layout/layout.phtml',
-        'application/index/index' => __DIR__ . '/view/application/index/index.phtml',
-        'error/404'               => __DIR__ . '/view/error/404.phtml',
-        'error/index'             => __DIR__ . '/view/error/index.phtml', 
+        //'application/index/index' => __DIR__ . '/view/application/index/index.phtml',
+        //'error/404'               => __DIR__ . '/view/error/404.phtml',
+        //'error/index'             => __DIR__ . '/view/error/index.phtml', 
     ),
 );
